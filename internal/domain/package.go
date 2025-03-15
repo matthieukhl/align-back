f3e300db91b34e7b1c01d0e303a83cb38a17f823
Matthieu Khairallah <matthieu.khairallah@gmail.com>
2025-03-15 13:50:20 +0100

    implemented package model

---
+++ internal/domain/package.go
@@ -0,0 +1,50 @@
+package domain
+
+import "time"
+
+// PackageType represents the type of package
+type PackageType string
+
+const (
+	GroupPackage   PackageType = "GROUP"
+	PrivatePackage PackageType = "PRIVATE"
+)
+
+// Package represents a Pilates session Package
+type Package struct {
+	ID               string      `json:"id" db:"id"`
+	Name             string      `json:"name" db:"name"`
+	NumberOfSessions int         `json:"number_of_sessions" db:"number_of_sessions"`
+	Type             PackageType `json:"type" db:"type"`
+	Price            float64     `json:"price" db:"price"`
+	CreatedAt        time.Time   `json:"created_at" db:"created_at"`
+	UpdatedAt        time.Time   `json:"updated_at" db:"updated_at"`
+}
+
+// PackageInput is used for creating/updating packages
+type PackageInput struct {
+	Name             string      `json:"name" validate:"required"`
+	NumberOfSessions int         `json:"number_of_sessions" validate:"required,min=1"`
+	Type             PackageType `json:"type" validate:"required,oneof=GROUP PRIVATE"`
+	Price            float64     `json:"price" validate:"required,min=0"`
+}
+
+// PackageRepository defines methods for package persistence
+type PackageRepository interface {
+	GetAll() ([]Package, error)
+	GetByID(id string) (*Package, error)
+	GetByType(pkgType PackageType) ([]Package, error)
+	Create(pkg *Package) error
+	Update(pkg *Package) error
+	Delete(id string) error
+}
+
+// PackageService defines business logic for packages
+type PackageService interface {
+	GetAll() ([]Package, error)
+	GetByID(id string) (*Package, error)
+	GetByType(pkgType PackageType) ([]Package, error)
+	Create(pkg *Package) error
+	Update(pkg *Package) error
+	Delete(id string) error
+}
